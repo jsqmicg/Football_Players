a818c496917620592da269f95a9cfe98b163ff4c
Juan Sebastian Quinonez Murillo <juansebastianquinonezmurillo@Mac-mini-de-Juan.local>
2021-06-28 16:58:14 -0500

    se sube aplicación

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Provider } from "react-redux";
+import EquiposSeleccionado from "./components/EquipoSeleccionado";
+import Jugadores from "./components/Jugadores";
+import "./styles/styles.scss";
+import store from "./store";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <main>
+        <h1>Football manager</h1>
+        <Jugadores />
+        <EquiposSeleccionado />
+      </main>
+    </Provider>
   );
-}
+};
 
 export default App;
---
+++ src/components/EquipoSeleccionado.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Suplentes from "./Suplentes";
+import Titulares from "./Titulares";
+
+const EquiposSeleccionado = () => (
+  <section>
+    <Titulares />
+    <Suplentes />
+  </section>
+);
+
+export default EquiposSeleccionado;
